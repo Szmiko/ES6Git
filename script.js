a96bd1357e80c31b3b738a9e22e1a1b909cf215c
Szymon Grabowski <szmiko@wp.pl>
2018-05-26 22:44:04 +0200

    Task

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
-
 class App extends React.Component {
 	constructor() {
 		super();
@@ -41,27 +36,33 @@ class App extends React.Component {
 		);
 	}
 }
-	class UsersList extends React.Component {
-		get users() {
-			return this.props.users.map(user => <User key={user.id} user={user}/>);
-		}
 
-		render() {
-			return (
-				<div>
-					{this.users}
-				</div>
-			);
-		}
+class UsersList extends React.Component {
+	get users() {
+		return this.props.users.map(user => <User key={user.id} user={user}/>);
 	}
 
-	class User extends React.Component {
-		render() {
-			return (
-				<div>
-					<img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
-					<a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
-				</div>
-    		);
-    	}
+	render() {
+		return (
+			<div>
+				{this.users}
+			</div>
+		);
 	}
+}
+
+class User extends React.Component {
+	render() {
+		return (
+			<div>
+				<img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
+				<a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
+			</div>
+    	);
+    }
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
